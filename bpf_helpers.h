9ee48d735a17b346dd0f540599e0a53660107c1b
Konstantin Belyalov <arsenicus@mail.ru>
2019-07-21 15:56:16 +0700

    [travis/clang-format] Add clang-format validation

--- bpf_helpers.h
+++ bpf_helpers.h
@@ -50,7 +50,7 @@ enum bpf_map_type {
 };
 
 /* flags for BPF_MAP_UPDATE_ELEM command */
-#define BPF_ANY     0 /* create new element or update existing */
+#define BPF_ANY 0     /* create new element or update existing */
 #define BPF_NOEXIST 1 /* create new element if it didn't exist */
 #define BPF_EXIST   2 /* update existing element */
 #define BPF_F_LOCK  4 /* spin_lock-ed map_lookup/map_update */
